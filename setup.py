2a23c61d87578eddc49278953fba4bbdd2d2e772
8me4ever <chenziming0624@gmail.com>
2024-10-03 04:55:03 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ DATA_FILES = []  # 如果有额外的资源文件，可以在这里添加
 OPTIONS = {
     'argv_emulation': True,  # 允许应用程序接收命令行参数
     'packages': ['pandas', 'tkinter', 'openpyxl', 'matplotlib'],  # 需要打包的库
-    'iconfile': 'icon.icns',  # 如果有自定义图标，可以添加
 }
 
 setup(
