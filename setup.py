93bcb6403175f94bf942c42fd57c38c5fceebd40
8me4ever <chenziming0624@gmail.com>
2024-10-03 05:01:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup
 APP = ['main.py']  # 你的主入口文件
 DATA_FILES = []  # 如果有额外的资源文件，可以在这里添加
 OPTIONS = {
-    'argv_emulation': True,  # 允许应用程序接收命令行参数
-    'packages': ['pandas', 'tkinter', 'openpyxl', 'matplotlib'],  # 需要打包的库
+    'argv_emulation': True,  # 启用命令行参数模拟
+    'packages': ['pandas', 'tkinter', 'openpyxl', 'matplotlib'],
+    'includes': ['PyInstaller.hooks.hook-gi.repository.GstWebRTC'],  # 显式包含缺失的模块
+    # 'iconfile': 'icon.icns',  # 如果没有 icns 文件，可以注释掉该行
 }
 
 setup(
