726024435a38b674bc012e25cb3365f269485ea6
8me4ever <chenziming0624@gmail.com>
2024-10-03 12:54:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from setuptools import setup
 APP = ['main.py']
 DATA_FILES = []
 OPTIONS = {
-    'argv_emulation': True,
+    'argv_emulation': True,  # 启用命令行参数模拟
     'packages': ['pandas', 'tkinter', 'openpyxl', 'matplotlib'],
-    'includes': ['PyQt5', 'PyQt5.QtQml', 'PyQt5.QtCore', 'PyQt5.QtGui', 'PyQt5.QtWidgets'],
-    'hookspath': ['./hooks'],  # 指定自定义 hook 文件路径
+    'includes': [
+        'PyQt5', 'PyQt5.QtCore', 'PyQt5.QtGui', 'PyQt5.QtWidgets', 'PyQt5.QtQml',
+        'PyInstaller.hooks.hook-PyQt5.QtQml',  # 显式指定导入问题的模块
+    ],
+    'excludes': ['PyInstaller.hooks.hook-PyQt5.QtQml'],  # 如果无法导入，可以尝试排除
 }
 
 setup(
